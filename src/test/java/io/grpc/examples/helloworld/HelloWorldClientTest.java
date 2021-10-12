6d3da40ebeb22da507960cdcff5b872350ef673b
yinlei <tongku2008@126.com>
2021-10-12 14:18:36 +0800

    使用新版的mockito，进行单元测试

--- src/test/java/io/grpc/examples/helloworld/HelloWorldClientTest.java
+++ src/test/java/io/grpc/examples/helloworld/HelloWorldClientTest.java
@@ -28,7 +28,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Matchers;
+import org.mockito.ArgumentMatchers;
 
 /**
  * Unit tests for {@link HelloWorldClient}.
@@ -70,7 +70,7 @@ public class HelloWorldClientTest {
     client.greet(testName);
 
     verify(serviceImpl)
-        .sayHello(requestCaptor.capture(), Matchers.<StreamObserver<HelloReply>>any());
+        .sayHello(requestCaptor.capture(), ArgumentMatchers.<StreamObserver<HelloReply>>any());
     assertEquals(testName, requestCaptor.getValue().getName());
   }
 }
