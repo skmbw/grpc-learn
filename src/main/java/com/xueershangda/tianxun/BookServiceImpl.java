d53ce8a46bcbd45aab0a8ca2af02450b056928ec
skmbw <tongku2008@126.com>
2021-10-13 16:56:14 +0800

    0.0.2的dubbo-compiler才有Dubbo3Generator这个代码生成器

--- src/main/java/com/xueershangda/tianxun/BookServiceImpl.java
+++ src/main/java/com/xueershangda/tianxun/BookServiceImpl.java
@@ -1,13 +1,13 @@
 package com.xueershangda.tianxun;
 
 import com.vteba.tutorial.Book;
-import com.vteba.tutorial.BookServiceDubbo;
+import com.vteba.tutorial.BookService;
 import com.vteba.tutorial.GetBookRequest;
 import com.vteba.tutorial.QueryBooksRequest;
 
 import java.util.concurrent.CompletableFuture;
 
-public class BookServiceImpl implements BookServiceDubbo.IBookService {
+public class BookServiceImpl implements BookService {
     @Override
     public Book getBook(GetBookRequest request) {
         return null;
